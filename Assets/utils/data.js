a7a6c0cd7deab8bdf96235068c88169e39fc83f3
Luke Ohde <lukeohde@Lukes-MacBook-Pro.local>
2023-02-15 05:56:15 -0700

    Complete

---
+++ Assets/utils/data.js
@@ -0,0 +1,131 @@
+const usernames = [
+    "Aaran",
+    "Aaren",
+    "Aarez",
+    "Aarman",
+    "Aaron",
+    "Aaron-James",
+    "Aarron",
+    "Aaryan",
+    "Aaryn",
+    "Aayan",
+    "Aazaan",
+    "Abaan",
+    "Abbas",
+    "Abdallah",
+    "Abdalroof",
+    "Abdihakim",
+    "Abdirahman",
+    "Abdisalam",
+    "Abdul",
+    "Abdul-Aziz",
+    "Abdulbasir",
+    "Abdulkadir",
+    "Abdulkarem",
+    "Smith",
+    "Jones",
+    "Coollastname",
+    "enter_name_here",
+    "Ze",
+    "Zechariah",
+    "Zeek",
+    "Zeeshan",
+    "Zeid",
+    "Zein",
+    "Zen",
+    "Zendel",
+    "Zenith",
+    "Zennon",
+    "Zeph",
+    "Zerah",
+    "Zhen",
+    "Zhi",
+    "Zhong",
+    "Zhuo",
+    "Zi",
+    "Zidane",
+    "Zijie",
+    "Zinedine",
+    "Zion",
+    "Zishan",
+    "Ziya",
+    "Ziyaan",
+    "Zohaib",
+    "Zohair",
+    "Zoubaeir",
+    "Zubair",
+    "Zubayr",
+    "Zuriel",
+    "Xander",
+    "Jared",
+    "Courtney",
+    "Gillian",
+    "Clark",
+    "Jared",
+    "Grace",
+    "Kelsey",
+    "Tamar",
+    "Alex",
+    "Mark",
+    "Tamar",
+    "Farish",
+    "Sarah",
+    "Nathaniel",
+    "Parker",
+  ];
+  const thoughts = [
+    "Decision Tracker",
+    "Find My Phone",
+    "Learn Piano",
+    "Starbase Defender",
+    "Tower Defense",
+    "Monopoly Money Manager",
+    "Movie trailers",
+    "Hello world",
+    "Stupid Social Media App",
+    "Notes",
+    "Messages",
+    "Email",
+    "Compass",
+    "Firefox",
+    "Running app",
+    "Cooking app",
+    "Poker",
+    "Deliveries",
+  ];
+  const reactions = ["💔", "❤️", "👍", "👎", "❗️"];
+  
+  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
+  const getRandomUsername = () => getRandomArrItem(usernames);
+  
+  const getRandomThoughtBody = () => getRandomArrItem(thoughts);
+  
+  const getRandomThought = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        thoughtText: getRandomArrItem(thoughts),
+        username: getRandomArrItem(usernames),
+        reactions: getRandomReactions(2),
+      });
+    }
+    return results;
+  };
+  const getRandomReactions = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+      results.push({
+        reactionBody: getRandomArrItem(reactions),
+        username: getRandomArrItem(usernames),
+      });
+    }
+    return results;
+  };
+  module.exports = {
+    getRandomArrItem,
+    getRandomUsername,
+    getRandomThought,
+    getRandomReactions,
+    getRandomThoughtBody,
+  };
---
+++ Assets/utils/seed.js
@@ -0,0 +1,44 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const {
+  getRandomArrItem,
+  getRandomUsername,
+  getRandomThought,
+  getRandomReactions,
+  getRandomThoughtBody,
+} = require("./data");
+connection.on("error", (err) => err);
+connection.once("open", async () => {
+  console.log("connected");
+  await Thought.deleteMany({});
+
+  await User.deleteMany({});
+  const usersArray = [];
+  const thoughtsArray = [];
+  for (let i = 0; i < 5; i++) {
+    const thoughts = getRandomThought(2);
+
+    const username = getRandomUsername();
+    const email = `${getRandomUsername()}@fakeemail.com`;
+    usersArray.push({
+      username,
+      email,
+      thoughts,
+    });
+
+    const thoughtText = getRandomThoughtBody();
+    const usernameThought = getRandomArrItem(usersArray);
+    const reactions = getRandomReactions(2);
+    thoughtsArray.push({
+      thoughtText,
+      username: usernameThought,
+      reactions,
+    });
+  }
+  await User.collection.insertMany(usersArray);
+  await Thought.collection.insertMany(thoughtsArray);
+  console.table(usersArray);
+  console.table(thoughtsArray);
+  console.info("Seeding Complete!");
+  process.exit(0);
+});
